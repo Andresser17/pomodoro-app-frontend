a1b267f127f32d191a34b3fe2a6c1e56e9ff31e2
Andresser17 <tensasangetsu1254@gmail.com>
2022-01-27 10:36:47 -0400

    Renamed Tasks.js to Timer.js, and Timer.js to Tasks.js.

--- src/sections/Tasks.js
+++ src/sections/Tasks.js
@@ -1,11 +1,91 @@
-function Tasks(props) {
+import { useState, useEffect } from "react";
+
+function SubTabs(props) {
+  // Default styles for subTab
+  let subTabStyles = "flex w-full bg-green-600 even:bg-yellow-600";
+  const [show, setShow] = useState("hidden");
+
+  const toggle = () => {
+    if (props.id === props.selected) {
+      setShow("");
+    } else setShow("hidden");
+  };
+
+  useEffect(() => {
+    toggle();
+  }, [props]);
+
+  return (
+    <div id={props.id} className={`${subTabStyles} ${show}`}>
+      {props.items.map((item, i) => (
+        <li key={i} className="mx-2">
+          <span className="underline cursor-pointer">{item}</span>
+        </li>
+      ))}
+    </div>
+  );
+}
+
+function Tabs(props) {
+  // States
+  const [selected, setSelected] = useState("pendingTasks");
+  let subTabs = [];
+
+  const tabs = props.tabs.map((item, i) => {
+    const tab = (
+      <span className="block p-2 text-white bg-red-600 cursor-pointer">
+        {item.tab}
+      </span>
+    );
+
+    // Save SubTabs
+    subTabs.push(
+      <SubTabs id={item.id} selected={selected} items={item.subTabs} />
+    );
+
+    return (
+      <li key={item.tab} onClick={() => setSelected(item.id)}>
+        {tab}
+      </li>
+    );
+  });
+
+  return (
+    <div className="">
+      <ul className="flex">{tabs}</ul>
+      {/* Tab content */}
+      {subTabs}
+    </div>
+  );
+}
+
+function List() {
+
+}
+
+function Tasks() {
+  const tabs = [
+    {
+      id: "pending-tasks",
+      tab: "Pending Tasks",
+      subTabs: ["Today", "Tomorrow"],
+    },
+    {
+      id: "completed-tasks",
+      tab: "Completed Tasks",
+      subTabs: ["Today", "Tomorrow"],
+    },
+  ];
   return (
     <div>
-      <button>Work</button>
-      <button>Short Break</button>
-      <button>Long Break</button>
-      <span>30:00</span>
-      <button>Start/Stop</button>
+      <Tabs tabs={tabs} />
+      {/* <SubMenu items={["Today", "Tomorrow"]} /> */}
+      {/* List of tasks */}
+      <div>
+        <ul>
+          <li>task 1</li>
+        </ul>
+      </div>
     </div>
   );
 }
--- src/sections/Timer.js
+++ src/sections/Timer.js
@@ -1,35 +1,11 @@
-function TimerMenu(props) {
-  const menu = props.items.map((item) => {
-    const key = Object.keys(item)[0];
-    const firstLevel = <span>{key}</span>;
-    const secondLevel = item[key].map((second) => <li>{second}</li>);
-
-    return (
-      <li className="">
-        {firstLevel} 
-        <ul>{secondLevel}</ul>
-      </li>
-    );
-  });
-
-  return <ul>{menu}</ul>;
-}
-
-function Timer() {
-  const menuItems = [
-    { "Pending Tasks": ["Today", "Tomorrow"] },
-    { "Completed Tasks": ["Today", "Tomorrow"] },
-  ];
+function Timer(props) {
   return (
     <div>
-      <TimerMenu items={menuItems} />
-      {/* <SubMenu items={["Today", "Tomorrow"]} /> */}
-      {/* List of tasks */}
-      <div>
-        <ul>
-          <li>task 1</li>
-        </ul>
-      </div>
+      <button>Work</button>
+      <button>Short Break</button>
+      <button>Long Break</button>
+      <span>30:00</span>
+      <button>Start/Stop</button>
     </div>
   );
 }
