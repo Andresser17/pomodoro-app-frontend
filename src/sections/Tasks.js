6b6074c44ba653634e27fe613abf7b767d16cc15
Andresser17 <tensasangetsu1254@gmail.com>
2022-01-27 11:57:04 -0400

    Defined TasksCard in Tasks.js

--- src/sections/Tasks.js
+++ src/sections/Tasks.js
@@ -16,7 +16,7 @@ function SubTabs(props) {
   }, [props]);
 
   return (
-    <div id={props.id} className={`${subTabStyles} ${show}`}>
+    <div className={`${subTabStyles} ${show}`}>
       {props.items.map((item, i) => (
         <li key={i} className="mx-2">
           <span className="underline cursor-pointer">{item}</span>
@@ -26,10 +26,25 @@ function SubTabs(props) {
   );
 }
 
+function TaskCard(props) {
+  const item = props.item;
+  return (
+    <div className={`${item.color} bg-red-600 p-4 my-4 w-96`}>
+      <div className="flex justify-between">
+        <h3 className="">{item.title}</h3>
+        <span className="">0/{item.pomodoros}</span>
+      </div>
+      <p>{item.description}</p>
+      <button>Change</button>
+    </div>
+  );
+}
+
 function Tabs(props) {
   // States
-  const [selected, setSelected] = useState("pendingTasks");
+  const [selected, setSelected] = useState(props.selectedByDefault);
   let subTabs = [];
+  let tasks = [];
 
   const tabs = props.tabs.map((item, i) => {
     const tab = (
@@ -40,8 +55,16 @@ function Tabs(props) {
 
     // Save SubTabs
     subTabs.push(
-      <SubTabs id={item.id} selected={selected} items={item.subTabs} />
+      <SubTabs
+        key={item.id}
+        id={item.id}
+        selected={selected}
+        items={item.subTabs}
+      />
     );
+
+    // Save Tasks
+    tasks.push(item.tasks.map((obj, j) => <TaskCard key={j} item={obj} />));
 
     return (
       <li key={item.tab} onClick={() => setSelected(item.id)}>
@@ -55,37 +78,40 @@ function Tabs(props) {
       <ul className="flex">{tabs}</ul>
       {/* Tab content */}
       {subTabs}
+      {/* List of tasks */}
+      <div className="bg-yellow-300">{tasks}</div>
     </div>
   );
 }
 
-function List() {
-
-}
-
 function Tasks() {
+  const taskSample = {
+    selected: false,
+    title: "Hello World",
+    description:
+      "Amet vero consequatur maiores ab assumenda Quas obcaecati voluptatem amet mollitia sed Maxime consequuntur at sequi a minima facilis.",
+    pomodoros: 3,
+    color: "#333",
+    completed: false,
+  };
   const tabs = [
     {
       id: "pending-tasks",
       tab: "Pending Tasks",
       subTabs: ["Today", "Tomorrow"],
+      tasks: [taskSample, taskSample],
     },
     {
       id: "completed-tasks",
       tab: "Completed Tasks",
       subTabs: ["Today", "Tomorrow"],
+      tasks: [taskSample, taskSample],
     },
   ];
+
   return (
     <div>
-      <Tabs tabs={tabs} />
-      {/* <SubMenu items={["Today", "Tomorrow"]} /> */}
-      {/* List of tasks */}
-      <div>
-        <ul>
-          <li>task 1</li>
-        </ul>
-      </div>
+      <Tabs selectedByDefault="pending-tasks" tabs={tabs} />
     </div>
   );
 }
